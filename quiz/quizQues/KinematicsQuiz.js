e47d8f9c3cdefda1e0c915946b36ecfb90ec535c
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2020-12-01 21:21:47 +0530

    adding kinematics quiz

---
+++ quiz/quizQues/KinematicsQuiz.js
@@ -0,0 +1,28 @@
+const quiz =[
+     {
+        q:'A juggler juggles 5 balls with two hands. Each ball rises 2 meters above her hands. Approximately how many times per minute does each hand toss a ball?',
+        options:['123times','117times','100times','112times'],
+        answer:1
+    },
+    {
+        q:`<img src="../quizQues/KinematicsQ1.png" width=100%;overflow="hidden">`,
+        options:['1.3s','2.5s','2.8s','3.2s'],
+        answer:0
+    },
+    {
+        q:'A river is flowing from west to east at a speed of 5 metre per minute. A man on the south bank of the river, capable of swimming at 10 metre per minute in still water, wants to swim across the river in the shortest time. He should swim in a direction. S',
+        options:['due north',' 30° east of north','30° west of north','60° east of north'],
+        answer:0
+    },
+    {
+        q:'A ball A ball of mass 0.2 kg rests on a vertical of heights 5 m. A bullet of mass 0.01 kg, travelling with a velocity V m/s in a horizontal direction, hits the centre of the ball. After the collision, the ball and bullet travel independently. They hits the ground at a distance of 20 m and the bullet at a distance of 100 m from the foot of the post. The initial velocity V of the bullet is',
+        options:['250 m/s','250√2 m/s','400 m/s','500 m/s'],
+        answer:3
+    },
+    {
+        q:'A particle moving in a straight line covers half the distance with speed of 3 m/s. The other half of the distance is covered in two equal time intervals with speed of 4.5 m/s and 7.5 m/s respectively. The average speed of the particle during this motion is ',
+        options:['4.0 m/s','5.0 m/s','5.5 m/s','4.8 m/s'],
+        answer:0
+    },
+    
+]
