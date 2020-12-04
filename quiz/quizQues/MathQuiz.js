a5a83882708fc8342cd8d44c95bf8c6c176b4f1d
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2020-12-04 21:34:45 +0530

    adding math questions js

---
+++ quiz/quizQues/MathQuiz.js
@@ -0,0 +1,50 @@
+const quiz =[
+    {
+       q:'If |z-4| < |z-2|, its solution is given by',
+       options:['Re(z)>0','Re(z)<0','Re(z)>3','Re(z)>2'],
+       answer:2
+   },
+   {
+       q:'The locus of the centre of a circle which touches the circle |z-z₁|=a and |z-z₂|=b externally will be',
+       options:['an ellipse','a hyperbola','a circle','none of these'],
+       answer:1
+   },
+   {
+       q:'If eˣ+e f(x)=e, then range of the function of f(x) is ',
+       options:['(- &#8734, 1]','(- &#8734, 1)','(&#8734,1)','[1,&#8734)'],
+       answer:3
+   },
+   {
+    q:'If f(x) is identity function, g(x) is absolute value function and h(x) is reciprocal function then',
+    options:['fogoh(x) = hogof(x)','hog(x) = hogof(x)','gofofofohogof(x) = gohog(x)','hohohoh(x) = f(x)'],
+    answer:2
+},
+
+{
+    q:'Let f(x) be a differentiable function and G be the graph of f(x). Let P = (a, f(a)) be a point on G closest to(0, 0). Then f(a)f(a) = ',
+    options:['a','-a','1','-1'],
+    answer:1
+},
+
+{
+    q:'The natural numbers are written as a sequence of digits 123456789101112 . . . , then in the sequence ',
+    options:['190th digit is 1','201st digit is 3','2014th digit is 8','2013th digit is same as 2014th digit'],
+    answer:1
+},
+
+{
+    q:'If a, b, c are three terms of an A.P. such that a ≠ b then (b – c)/(a – b) may be equal to',
+    options:['0','√3','1','2'],
+    answer:1
+},
+
+{
+    q:'The lines 5x + 12y - 10 = 0 and 5x - 12y - 40 = 0 touch a circle of radius 3 units. If the centre of circle lies in the first quadrant, then the coordinates of centre is',
+    options:['(5,3)','(5,1)','(5,2)','(5,6)'],
+    answer:2
+},
+
+
+   
+   
+]
