505a977d77df9b92f2be020e14cfe03edca7ee52
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2020-12-04 20:55:40 +0530

    adding Chemistry ques in js

---
+++ quiz/quizQues/ChemistryQuiz.js
@@ -0,0 +1,43 @@
+const quiz =[
+    {
+       q:'According to the kinetic theory of gases, in  an ideal gas, between two successive collisions a gas molecule travels',
+       options:['in a wavy path','in a straight line path','with an accelerated velocity','in a circular path'],
+       answer:1
+   },
+   {
+       q:'In Van der Waals equation of state of the gas law, the constant ‘b’ is a measure of',
+       options:['intermolecular repulsions','intermolecular collisions per unit volume','Volume occupied by the molecules','intermolecular attraction'],
+       answer:2
+   },
+   {
+       q:'Which one of the following statements is NOT true about the effect of an increase in temperature on the distribution of molecular speeds in a gas ?',
+       options:['The most probable speed increases','The fraction of the molecules with the most probable speed increases','The distribution becomes broader','The area under the distribution curve remains the same as under the lower temperature'],
+       answer:1
+   },
+   {
+       q:'Copper crystallizes in fee with a unit cell length of 361 pm. What is the radius of copper atom?',
+       options:['108ppm','250ppm','300ppm','127ppm'],
+       answer:3
+   },
+   {
+       q:'The compressibility factor for a real gas at high pressure is ',
+       options:['1+RT/Pb','1','1+Pb/RT','1-Pb/RT'],
+       answer:2
+   },
+   {
+    q:'Which of the following exists as covalent crystals in the solid state?',
+    options:['Iodine','Silicon','Sulphur','Phosphorus'],
+    answer:1
+},
+{
+    q:'Which of the following statements is true?',
+    options:['HF is less polar than HBr','absolutely pure water does not contain any ions','chemical bond formation take place when forces of attraction overcome the forces of repulsion','in covalency transference of electron takes place'],
+    answer:2
+},
+{
+    q:'Lattice energy of an ionic compounds depends upon  ',
+    options:['Chaige on the ion only','Size of the ion only','Packing of ions only','Charge on the ion and size of the ion'],
+    answer:3
+},
+   
+]
